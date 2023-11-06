f545de998064256f2b04bed0eef6123f5b2337ce
Thenmozhi <126195333+Thenmozhi-k@users.noreply.github.com>
2023-11-06 23:31:33 +0530

    Add files via upload

---
+++ landing-page/script.js
@@ -0,0 +1,15 @@
+/*
+document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach((card, index) => {
+        card.addEventListener("mouseover", () => {
+            card.style.transform = `scale(1.3) translatex(${index * -50}px)`;
+        });
+
+        card.addEventListener("mouseout", () => {
+            card.style.transform = `translateX(${index * -50}px)`;
+        });
+    });
+});
+*/
